3f4fe7c67fdf4d071660e3e6ed04133fc4f33e26
shhmd92 <sho.hamada.92@gmail.com>
2020-09-08 19:27:31 +0900

    [mod]スキル情報をv-forで設定するように修正

--- src/views/Skill.vue
+++ src/views/Skill.vue
@@ -1,56 +1,67 @@
 <template>
-  <section>
-    <v-container fluid fill-height>
-      <v-row class="grey lighten-4" justify="center" align-content="center">
-        <v-col>
-          <v-card class="lighten-3 ma-2">
-            <v-card-title class="justify-center">Language</v-card-title>
-            <v-card-text>
-              <div class="text--primary">
-                <ul>
-                  <li>Ruby</li>
-                  <li>Java</li>
-                  <li>JavaScript</li>
-                </ul>
-              </div>
-            </v-card-text>
-          </v-card>
-        </v-col>
-        <v-col>
-          <v-card class="lighten-3 ma-2">
-            <v-card-title class="justify-center">Framework</v-card-title>
-            <v-card-text>
-              <div class="text--primary">
-                <ul>
-                  <li>Ruby on Rails</li>
-                  <li>Struts</li>
-                  <li>Vue.js</li>
-                </ul>
-              </div>
-            </v-card-text>
-          </v-card>
-        </v-col>
-        <v-col>
-          <v-card class="lighten-3 ma-2">
-            <v-card-title class="justify-center">Others</v-card-title>
-            <v-card-text>
-              <div class="text--primary">
-                <ul>
-                  <li>AWS</li>
-                  <li>Oracle</li>
-                  <li>MySQL</li>
-                </ul>
-              </div>
-            </v-card-text>
-          </v-card>
-        </v-col>
-      </v-row>
-    </v-container>
-  </section>
+  <v-container fluid>
+    <h2 class="content-title">SKILL</h2>
+    <v-row class="grey lighten-4" justify="center" align-content="center">
+      <v-col v-for="(skill, index) in skills" :key="index">
+        <v-card class="lighten-3 ma-2">
+          <v-card-title class="justify-center">{{ skill.label }}</v-card-title>
+          <v-card-text>
+            <div class="text--primary">
+              <ul>
+                <li v-for="(skillItem, index) in skill.item" :key="index">
+                  {{ skillItem }}
+                </li>
+              </ul>
+            </div>
+          </v-card-text>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
 export default {
   name: "Skill",
+  data() {
+    return {
+      skills: [
+        {
+          label: "Language",
+          item: ["Ruby", "Java", "JavaScript"],
+        },
+        {
+          label: "Framework",
+          item: ["Ruby on Rails", "Struts", "Vue.js"],
+        },
+        {
+          label: "Others",
+          item: ["Git", "GitHub", "AWS", "Oracle", "MySQL"],
+        },
+      ],
+    };
+  },
 };
 </script>
+
+<style lang="scss" scoped>
+ul {
+  list-style: none;
+  padding: 0px;
+
+  li {
+    box-sizing: border-box;
+    padding: 15px;
+    text-align: center;
+    min-width: 50%;
+
+    &:nth-child(odd) {
+      background-color: #eef;
+    }
+
+    &:nth-child(even) {
+      background-color: #fee;
+    }
+  }
+}
+</style>
