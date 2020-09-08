a134b6ee878e6833e3b3669395fa25ab68ef67b8
shhmd92 <sho.hamada.92@gmail.com>
2020-09-08 19:25:24 +0900

    [mod]スキル一覧の追加

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,9 +1,49 @@
 <template>
-  <div></div>
+  <v-container fluid>
+    <h2 class="content-title">ABOUT</h2>
+    <div>
+      <table class="mx-auto">
+        <tr v-for="(profile, index) in profiles" :key="index">
+          <td class="pr-2">
+            {{ profile.key }}
+          </td>
+          <td v-html="profile.value">{{ profile.value }}</td>
+        </tr>
+      </table>
+    </div>
+  </v-container>
 </template>
 
 <script>
 export default {
   name: "About",
+  data() {
+    return {
+      profiles: [
+        {
+          key: "Name",
+          value: "Sho",
+        },
+        {
+          key: "University",
+          value: "KUT(Kochi University of Technology)",
+        },
+        {
+          key: "GitHub",
+          value:
+            "<a href='https://github.com/shhmd92' target='_blank' rel='noopener noreferrer'>shhmd92</a>",
+        },
+        {
+          key: "Qiita",
+          value:
+            "<a href='https://qiita.com/shhmd92' target='_blank' rel='noopener noreferrer'>shhmd92</a>",
+        },
+        {
+          key: "Twitter",
+          value: "private(to be public?)",
+        },
+      ],
+    };
+  },
 };
 </script>
