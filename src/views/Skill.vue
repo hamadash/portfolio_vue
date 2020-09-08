aa623614ee55db31f142ed8dde2245e4884cac6c
shhmd92 <sho.hamada.92@gmail.com>
2020-09-08 12:53:46 +0900

    [add]スキル一覧の作成

--- src/views/Skill.vue
+++ src/views/Skill.vue
@@ -1,5 +1,52 @@
 <template>
-  <div></div>
+  <section>
+    <v-container fluid fill-height>
+      <v-row class="grey lighten-4" justify="center" align-content="center">
+        <v-col>
+          <v-card class="lighten-3 ma-2">
+            <v-card-title class="justify-center">Language</v-card-title>
+            <v-card-text>
+              <div class="text--primary">
+                <ul>
+                  <li>Ruby</li>
+                  <li>Java</li>
+                  <li>JavaScript</li>
+                </ul>
+              </div>
+            </v-card-text>
+          </v-card>
+        </v-col>
+        <v-col>
+          <v-card class="lighten-3 ma-2">
+            <v-card-title class="justify-center">Framework</v-card-title>
+            <v-card-text>
+              <div class="text--primary">
+                <ul>
+                  <li>Ruby on Rails</li>
+                  <li>Struts</li>
+                  <li>Vue.js</li>
+                </ul>
+              </div>
+            </v-card-text>
+          </v-card>
+        </v-col>
+        <v-col>
+          <v-card class="lighten-3 ma-2">
+            <v-card-title class="justify-center">Others</v-card-title>
+            <v-card-text>
+              <div class="text--primary">
+                <ul>
+                  <li>AWS</li>
+                  <li>Oracle</li>
+                  <li>MySQL</li>
+                </ul>
+              </div>
+            </v-card-text>
+          </v-card>
+        </v-col>
+      </v-row>
+    </v-container>
+  </section>
 </template>
 
 <script>
